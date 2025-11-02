9db1fda75580a4b036eb95de36d8bb7078b7e875
kamrul-hasan960 <hasankamrul06723@gmail.com>
2025-11-02 14:50:02 +0600

    Lab task 1 Clinic patient Registration

--- mid/Lab Task 1/index.php
+++ mid/Lab Task 1/index.php
@@ -1,11 +1,96 @@
 <!DOCTYPE html>
 <html>
-<body>
+        <style>
+        body {
+            background-color: #D2B48C; 
+            font-family: Arial, sans-serif;
+        }
+        table {
+            background-color: DarkGoldenRod ;
+            padding: 20px 40px;
+            border-radius: 10px;
+        }
+        button {
+            background-color: black;
+            color: white;
+           padding: 10px 50px;
+            border: none;
+        }
+       
+    </style>
+    <body>
+        <center>
+            <h1>Clinic patient Registration</h1>
+        <br><br>
+        <table>
+            <tr>
+                <td>Full Name:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+                <td>Age:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+                <td>Phone Number:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+                <td>Insurance Provider:</td>
+            </tr>
+             <tr>
+                <td>
+                    <select>
+                        <option> select Provider</option>
+                    </select>
+                </td>
+            </tr>
+             <tr>
+                <td>Insurance Polycy Number:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+            <tr>
+                <td><center>
+                   <h4>Additional Information</h4> 
+                </center></td>
+            </tr>
+            <br>
+             <tr>
+                <td>Username:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+                 <tr>
+                <td>Pasword:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+                 <tr>
+                <td>Confirm Password:</td>
+            </tr>
+             <tr>
+                <td><input type = "text"></td>
+            </tr>
+             <tr>
+            <tr><td><center> <button type="button">Register</center></td> </tr>  
+           
+ 
+        </table>
 
-<h2>The src Attribute</h2>
-<p>HTML images are defined with the img tag, and the filename of the image source is specified in the src attribute:</p>
-
-<img src="img_girl.jpg" width="500" height="600">
-
-</body>
+      </center>
+      <button
+    </body>
 </html>
