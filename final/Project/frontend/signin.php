06b7a37cc469ea5387b083f60eedc6ec12e59f6c
kamrul-hasan960 <hasankamrul06723@gmail.com>
2025-12-21 00:19:27 +0600

    Implement SignIn page

---
+++ final/Project/frontend/signin.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign In</title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a>Homepage</a></li>
+                <li><a>Browse a Job</a></li>
+                <li><a>Browse a Talent</a></li>
+                <li><a>Contact Us</a></li>
+                <li><a>Sign In</a></li>
+                <li><a>Sign Up</a></li>
+            </ul>
+        </nav>
+        <h2>Sign In</h2>
+        <form>
+            <label>Userename or Email</label>
+            <input type = "text">
+            <label>Password</label>
+            <input type = "Password">
+            <div>
+                <a>Forgot Password</a>
+            </div>
+            <button type = "submit">Sign In</button>
+        </form>
+        <p>Don't have an account?<a>Sign Up</a></p>
+
+        <footer>
+            <div class = "container">
+                <div class = "footer-content">
+                    <div class = "footer_section">
+                        <h3>About</h3>
+                        <a>About Us</a>
+                        <p>Nilkham is a platform connecting talented researchers and developers with oppurtunities.</p>
+                    </div>
+                    <div class = "footer_section">
+                        <h3>Location</h3>
+                        <p>Dhaka, Bangladesh</p>
+                    </div>
+                    <div class = "footer_section">
+                        <h3>Our Policy</h3>
+                        <a>Privacy Policy</a>
+                        <a>Terms of services</a>
+                    </div>
+                    <div class = "footer_section">
+                       <h3>Help Center</h3>
+                       <a>Contact Us</a>
+                       <a>FAQ</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
